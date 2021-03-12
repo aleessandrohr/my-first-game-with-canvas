0040d3ac70af3d3fbc6e6a26d9d94f5f3c55fa28
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-03-12 11:55:40 -0300

    update keyboardListener

--- src/keyboardListener.ts
+++ src/keyboardListener.ts
@@ -1,28 +1,15 @@
-import { createObservers } from "./observers.js";
-
-export const createKeyboardListener = () => {
+export const createKeyboardListener = (notifyAll: (command: any) => void) => {
 	let playerId: string | null = null;
 
 	const registerPlayerId = (id: string) => {
 		playerId = id;
 	};
 
-	const unregisterPlayerId = () => {
-		playerId = null;
-	};
-
-	const {
-		subscribe,
-		unsubscribe,
-		unsubscribeAll,
-		notifyAll,
-	} = createObservers();
-
 	const handleKeydown = (event: KeyboardEvent) => {
 		const keyPressed = event.key;
 
 		const command = {
-			type: "key-pressed",
+			type: "command",
 			playerId,
 			key: keyPressed,
 		};
@@ -32,11 +19,5 @@ export const createKeyboardListener = () => {
 
 	document.addEventListener("keydown", handleKeydown);
 
-	return {
-		registerPlayerId,
-		unregisterPlayerId,
-		subscribe,
-		unsubscribe,
-		unsubscribeAll,
-	};
+	return { registerPlayerId };
 };
