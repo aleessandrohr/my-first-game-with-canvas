93a2ac7fce196714aa3326b5a697edf7b5eb8e96
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-03-14 14:40:04 -0300

    update game

--- src/game.ts
+++ src/game.ts
@@ -1,11 +1,11 @@
-import { createObservers } from "./observers.js";
-import { Canvas } from "./types/interfaces/Canvas";
-import { State } from "./types/interfaces/State";
-import { AddObject } from "./types/interfaces/AddObject";
-import { RemoveObject } from "./types/interfaces/RemoveObject";
-import { HandleCommands } from "./types/interfaces/HandleCommands";
-import { AcceptedCommands } from "./types/interfaces/AcceptedCommands";
-import { Player } from "./types/interfaces/Player";
+import { createObservers } from "@/observers.js";
+import { AcceptedCommands } from "@/types/interfaces/AcceptedCommands";
+import { AddObject } from "@/types/interfaces/AddObject";
+import { Canvas } from "@/types/interfaces/Canvas";
+import { HandleCommands } from "@/types/interfaces/HandleCommands";
+import { IObject } from "@/types/interfaces/Object";
+import { RemoveObject } from "@/types/interfaces/RemoveObject";
+import { State } from "@/types/interfaces/State";
 
 export const createGame = () => {
 	const canvas: Canvas = {
@@ -18,11 +18,11 @@ export const createGame = () => {
 				width: 1,
 				height: 1,
 			},
-			colors: {
-				currentPlayer: "#f0db4f",
-				players: "black",
-				fruits: "green",
-			},
+		},
+		colors: {
+			currentPlayer: "#f0db4f",
+			players: "black",
+			fruits: "green",
 		},
 	};
 
@@ -35,17 +35,6 @@ export const createGame = () => {
 		Object.assign(state, newState);
 	};
 
-	const start = (frequency?: number, fruitNumbers?: number) => {
-		const currentFrequency = frequency ? frequency : 5000;
-		const currentFruitNumbers = fruitNumbers ? fruitNumbers : 10;
-
-		setInterval(() => {
-			const fruitId = Math.floor(Math.random() * currentFruitNumbers);
-
-			addObjet({ key: "fruits", id: fruitId });
-		}, currentFrequency);
-	};
-
 	const { subscribe, unsubscribe, notifyAll } = createObservers();
 
 	const addObjet = ({ key, id, x, y }: AddObject) => {
@@ -54,11 +43,6 @@ export const createGame = () => {
 		const positionY =
 			y || y === 0 ? y : Math.floor(Math.random() * canvas.screen.height);
 
-		state[key][id] = {
-			x: positionX,
-			y: positionY,
-		};
-
 		notifyAll({
 			type: "add-object",
 			key,
@@ -66,29 +50,29 @@ export const createGame = () => {
 			x: positionX,
 			y: positionY,
 		});
+
+		state[key][id] = {
+			x: positionX,
+			y: positionY,
+		};
 	};
 
 	const removeObject = ({ key, id }: RemoveObject) => {
-		delete state[key][id];
-
 		notifyAll({
 			type: "remove-object",
 			key,
 			id,
 		});
+
+		delete state[key][id];
 	};
 
-	const handleCommands = ({ type, playerId, key }: HandleCommands) => {
-		const player = state.players[playerId];
+	const checkForFruitCollision = (player: IObject) => {
+		for (const fruitId in state.fruits) {
+			const fruit = state.fruits[fruitId];
 
-		if (player) {
-			const command = key.toLowerCase();
-			const commandFunction = acceptedCommands[command];
-
-			if (commandFunction) {
-				commandFunction(player);
-				checkForFruitCollision(player);
-				notifyAll({ type, playerId, key });
+			if (player.x === fruit.x && player.y === fruit.y) {
+				removeObject({ key: "fruits", id: fruitId });
 			}
 		}
 	};
@@ -136,14 +120,31 @@ export const createGame = () => {
 		},
 	};
 
-	const checkForFruitCollision = (player: Player) => {
-		for (const fruitId in state.fruits) {
-			const fruit = state.fruits[fruitId];
+	const handleCommands = ({ type, playerId, key }: HandleCommands) => {
+		notifyAll({ type, playerId, key });
 
-			if (player.x === fruit.x && player.y === fruit.y) {
-				removeObject({ key: "fruits", id: fruitId });
+		const player = state.players[playerId];
+
+		if (player) {
+			const command = key.toLowerCase();
+			const commandFunction = acceptedCommands[command];
+
+			if (commandFunction) {
+				commandFunction(player);
+				checkForFruitCollision(player);
 			}
 		}
+	};
+
+	const start = (frequency?: number, fruitNumbers?: number) => {
+		const currentFrequency = frequency ? frequency : 5000;
+		const currentFruitNumbers = fruitNumbers ? fruitNumbers : 15;
+
+		setInterval(() => {
+			const fruitId = Math.floor(Math.random() * currentFruitNumbers);
+
+			addObjet({ key: "fruits", id: fruitId });
+		}, currentFrequency);
 	};
 
 	return {
